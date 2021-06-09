578d7bef45e840e713f5ca52e1a3ea9a8e77bfcc
Radmir92 <v-radmir92@mail.ru>
2021-06-09 20:18:01 +0500

    added PageObjects

--- src/test/java/StudentRegistrationPage.java
+++ src/test/java/StudentRegistrationPage.java
@@ -5,7 +5,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class StudentRegistrationPage {
+public class PageObjects {
 
     public static void openPage(String url) {
         open(url);
